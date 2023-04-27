48b3732464aa3d83b752e3aaa29a564743970f91
chrismdating1206@gmail.com <christian.dating12>
2023-04-27 20:58:54 +0800

    intro

--- Program 3.py
+++ Program 3.py
@@ -1,3 +1,11 @@
 # Chistian Dating | BSCPE 1-5
 
 # Write a method in python to write multiple line of text contents into a text file mylife.txt
+
+input("\n\033[94mPress Any Key to Start...")
+print("\033[90m=" * 80)
+
+# Intro
+import pyfiglet
+greet = "GOOD DAY!"
+print("\033[92m" + pyfiglet.figlet_format(greet, font = "Thin"))
